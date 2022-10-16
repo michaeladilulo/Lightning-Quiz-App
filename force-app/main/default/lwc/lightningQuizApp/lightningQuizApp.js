60bbe33e4f7b762aa50c2cdd0cddd06216b08aad
michaeladilulo <michaeladilulo@gmail.com>
2022-10-16 14:33:57 -0600

    Questions Render to the page, hard coded questions

--- force-app/main/default/lwc/lightningQuizApp/lightningQuizApp.js
+++ force-app/main/default/lwc/lightningQuizApp/lightningQuizApp.js
@@ -1,3 +1,39 @@
 import { LightningElement } from 'lwc';
 
-export default class LightningQuizApp extends LightningElement {}
+export default class LightningQuizApp extends LightningElement {
+    examQuestions = [
+        {
+            id: 'Question One',
+            question: 'What is Maya?',
+            answers: {
+                a: 'Dog',
+                b: 'Cat',
+                c: 'Kangaroo',
+                d: 'Giraffe'
+            },
+            correctAnswer: 'a'
+        },
+        {
+            id: 'Question Two',
+            question: "What is Maya's favorite thing?",
+            answers: {
+                a: 'Treats',
+                b: 'Getting a bath',
+                c: 'Walks',
+                d: 'Taking medication'
+            },
+            correctAnswer: 'a'
+        },
+        {
+            id: 'Question Three',
+            question: 'When dad comes home, how is Maya?',
+            answers: {
+                a: 'Tired',
+                b: 'Sleepy',
+                c: 'Difficult',
+                d: 'Super excited'
+            },
+            correctAnswer: 'd'
+        }
+    ]
+}
